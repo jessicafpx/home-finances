c357c0989f8b46a16868f8e08c95223178132a46
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2024-02-18 18:55:20 -0300

    feature: update feedback list transactions

--- src/services/transactions/hooks/GET/useListTransactions.ts
+++ src/services/transactions/hooks/GET/useListTransactions.ts
@@ -1,14 +1,19 @@
+import { useToast } from "@/hooks/useToast";
 import { globalQueryKeys } from "@/services/config/queryKeys";
 import { api } from "@/services/endpoints";
 import { useQuery } from "react-query";
 
 export const useListTransactions = () => {
+  const { addToast } = useToast();
   const { data } = useQuery(
     [globalQueryKeys["useListTransactions"]],
     () => api.getTransactions(),
     {
       onError: () => {
-        console.log("Erro ao buscar as transações disponíveis");
+        addToast({
+          title: "Erro ao buscar as transações disponíveis",
+          type: "error",
+        });
       },
     }
   );
