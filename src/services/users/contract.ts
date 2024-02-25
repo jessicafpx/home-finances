f5a7b32c042ef0854da5f585763fbc83e0bfdd00
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2024-02-25 16:22:55 -0300

    feat: update User contract

--- src/services/users/contract.ts
+++ src/services/users/contract.ts
@@ -1,6 +1,22 @@
-interface User {
+import { TransactionTypeEnum } from "../transactions/contract";
+
+export interface Category {
   name: string;
+  percent: number | null;
+  type: TransactionTypeEnum;
   id: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
   email: string;
   password: string;
+  totalIncome: number;
+  categories: Category[] | null;
 }
+
+export type TUseUpdateUserOptions = {
+  onSuccess?: () => void;
+  onError?: (error: unknown) => void;
+};
