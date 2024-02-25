00acb4c1f16132ec245e70cb217517fbc5f439a8
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2024-02-25 16:23:47 -0300

    feat: add progress template

--- src/pages/progress/index.tsx
+++ src/pages/progress/index.tsx
@@ -1,9 +1,21 @@
 import Navbar from "@/components/Navbar";
+import { IconProgress } from "@/components/icons";
+import * as S from "@/components/styles/pages.styles";
 
 export default function Progress() {
   return (
-    <div>
+    <S.Wrapper>
       <Navbar />
-    </div>
+      <S.Content>
+        <S.Header>
+          <S.TitleContainer>
+            <S.IconBox>
+              <IconProgress color="#2E117B" size={24} />
+            </S.IconBox>
+            <S.Title>Progresso</S.Title>
+          </S.TitleContainer>
+        </S.Header>
+      </S.Content>
+    </S.Wrapper>
   );
 }
